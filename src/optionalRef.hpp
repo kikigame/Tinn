1ba82a4e6f6c67f28bd4f7641d39aa9be624a1d3
Robert J Lee <github@rjlee.homelinux.org>
2017-12-05 23:53:55 +0000

    Optianal reference, based on C++18's std::optional, but with better support for references. I prefer to use this to shared_ptr<item> for optional items as the intention is clearer. -RL

---
+++ src/optionalRef.hpp
@@ -0,0 +1,28 @@
+/* License and copyright go here*/
+
+// Places where items are
+
+#ifndef OPTIONAL_REF__
+#define OPTIONAL_REF__
+
+// basically the same as in C++17, but I don't want to up the standard for one template class
+// this one only ever holds a reference
+template <typename T>
+class optionalRef {
+private:
+  bool has_value_;
+  T* value_;
+public:
+  optionalRef() : has_value_(false), value_(0) {}
+  optionalRef(T & t) : has_value_(true), value_(&t) {};
+  optionalRef<T> &operator =(const optionalRef<T> &other) {
+    has_value_ = other.has_value_;
+    value_ = other.value_;
+    return *this;
+  }
+  operator bool() const { return has_value_; };
+  T & value() {return *value_; }
+  void reset() { has_value_ = false; value_ = 0; }
+};
+
+#endif // ndef OPTIONAL_REF__
