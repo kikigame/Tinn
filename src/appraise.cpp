8746da6114f86ec9ccf6c8eaf701ac4b3b6d4e4d
Robert J Lee <github@rjlee.homelinux.org>
2017-12-08 21:17:12 +0000

    Adjust value based on item category -RL

--- src/appraise.cpp
+++ src/appraise.cpp
@@ -6,10 +6,38 @@
 #include "damage.hpp"
 #include "monster.hpp"
 
+bool isDamaged(const item &);
+
 double appraise(const monster &monster, const item &thing) {
 
   auto value = thing.weight();
-  // TODO: switch on category type
+  // switch on category type
+  switch (thing.render()) {
+    // food
+  case L'%': value *= 0.5; break;
+    // weapons
+  case L'!': value *= 10; break;
+    // missiles
+  case L'¬': value *= 3; break;
+    // clothing - worth much less if damaged
+  case L'[': if (isDamaged(thing)) value *= 0.1; break;
+    // stylii - worth lots if magical
+  case L'/': if (thing.enchantment() > 0) value *= 10; break;
+    // bottles
+  case L'8': break;
+    // readables
+  case L'¶': break;
+    // containers
+  case L'=': break;
+    // Liquids
+  case L'~': break;
+    // jewellery - lightweight but more valuable
+  case L'*': value *= 30; break;
+    // valuables - inherantly valuable
+  case L'$': value *= 40; break;
+    // tools - utility value
+  case L'(': value *= 2; break;
+  }
   if (thing.isBlessed()) value *= 2;
   if (thing.isCursed()) value *= 0.5;
   if (thing.isUnidentified()) value *= 0.9;
@@ -24,4 +52,12 @@ double appraise(const monster &monster, const item &thing) {
   value += monster.appearance().cur(); // TODO: buy/sell; tweak
   value *= (100 + thing.enchantment()) / 100.;
   return value;
+}
+
+
+bool isDamaged(const item &item) {
+  auto &dr = damageRepo::instance();
+  for (auto dt : dr) // map of damageType to damage
+    if (item.damageOfType(dt.first) != 0) return true;
+  return false;
 }
