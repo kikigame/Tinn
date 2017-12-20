66525cfec7ed271c83ee7082fd8dfe7197ac0e2d
Robert J Lee <github@rjlee.homelinux.org>
2017-12-20 19:16:22 +0000

    Actions are now renderable, so they appear in the guide. This will also give us a name for wands, etc. -RL

--- src/action.cpp
+++ src/action.cpp
@@ -7,11 +7,12 @@
 #include <map>
 
 // source steals an item from target
-class steal : public sharedAction<monster, monster> {
+class steal : public renderedAction<monster, monster> {
 private:
   std::function<bool(item &)> f_;
 public:
-  steal(std::function<bool(item &)> f) : f_(f) {};
+  steal(const wchar_t * const name, const wchar_t * const description, std::function<bool(item &)> f) : 
+    renderedAction(name, description), f_(f) {};
   virtual ~steal() {};
   bool operator ()(monster &source, monster &target) {
     auto &io = ioFactory::instance();
@@ -46,17 +47,48 @@ public:
   }
 };
 
+// basic weapon damage action
+template<unsigned char damagePc>
+class attackRay : public renderedAction<monster, monster> {
+private:
+  damageType damageType_;
+public:
+  attackRay(const wchar_t * const name, const wchar_t * const description, damageType type) :
+    renderedAction(name, description), damageType_(type) {};
+  virtual ~attackRay() {};
+  bool operator ()(monster &source, monster &target) {
+    auto &d = damageRepo::instance()[damageType_];
+    int damage = target.wound(damagePc, d);
+    auto &io = ioFactory::instance();
+    bool rtn = damage > 0;
+    if (!rtn)
+      io.message(std::wstring(L"The ") + std::wstring(d.name()) + L" ray fires harmlessly as " + target.name());
+    else 
+      io.message(std::wstring(d.name()) + L" takes " + target.name() + L" damage.");
+    return rtn;
+  }
+};
+
 template<>
 class actionFactory<monster, monster> {
 public:
   typedef sharedAction<monster, monster> action;
   static const action &get(const typename action::key k) {
     static std::map<action::key, std::shared_ptr<action> > m{
-      std::make_pair<action::key, std::shared_ptr<action> >
-	(action::key::steal_small, std::shared_ptr<action>(new steal([](item &i) { return i.weight() <= 1; }))),
-      std::make_pair<action::key, std::shared_ptr<action> >
-	(action::key::steal_shiny, std::shared_ptr<action>(new steal([](item &i) { return i.render() == L'*' || i.render() == L'$'; })))
-    };
+      std::make_pair(action::key::steal_small, std::shared_ptr<action>(new steal(L"pickpocketing",
+L"Pickpocketing is an act of petty larceny.\n"
+"It does not require actual pockets; indeed, objects placed in containers may\n"
+"evade the scope of this activity, as are those which are too large - say,\n"
+"bigger than an apple.",
+[](item &i) { return i.weight() <= 1; }))),
+      std::make_pair(action::key::steal_shiny, std::shared_ptr<action>(new steal(L"petty larcony",
+L"Larcony - or theft - is the act of taking items from another for personal\n"
+"gain. This applies to jewellery and other valuables.",
+[](item &i) { return i.render() == L'*' || i.render() == L'$'; }))),
+	std::make_pair(action::key::attack_ray_small_water, std::shared_ptr<action>(new attackRay<5>(L"water jet",
+L"A water jet will squirt a target with a small amount of water.\n",
+damageType::wet)))
+	};
     return *(m[k]);
   };
 };
--- src/action.hpp
+++ src/action.hpp
@@ -6,6 +6,8 @@
 #ifndef ACTION_HPP__
 #define ACTION_HPP__
 
+#include "renderable.hpp"
+
 class item;
 class monster;
 
@@ -13,6 +15,7 @@ class monster;
 template <class ...T>
 class sharedAction {
 public:
+  virtual ~sharedAction() {};
   // takes targets; returns true if action was successful, false if blocked or failed.
   virtual bool operator ()(T & ... ) = 0;
 };
@@ -28,8 +31,24 @@ class sharedAction<monster, monster>{
     steal_small,
     // steal treasure item (category '*')
     steal_shiny,
+    // water ray 5%
+      attack_ray_small_water,
     END
   };
+};
+
+template<class ...T>
+class renderedAction : public sharedAction<T...>, public renderable {
+private:
+  const wchar_t * const name_;
+  const wchar_t * const description_;
+public:
+renderedAction(const wchar_t * const name, const wchar_t * const description) :
+  sharedAction<T...>(), name_(name), description_(description) {}
+  virtual bool operator()(T &...) = 0;
+  virtual const wchar_t render() const { return L'☇'; } // lightning bolt for magic
+  virtual const wchar_t * const name() const { return name_; };
+  const wchar_t * const description() const { return description_; };
 };
 
 template<class ...T>
