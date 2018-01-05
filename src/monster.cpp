377004f6d5b85b960ee5c8b6a3c813116b1e656c
Robert J Lee <github@rjlee.homelinux.org>
2018-01-05 21:37:36 +0000

    Bugfix: When a ferret, goblin, or other stealing monster dealt a death blow, the onHit() action was called with an expired reference to the monster, causing a SIGSEGV. Fixed. -RL

--- src/monster.cpp
+++ src/monster.cpp
@@ -123,9 +123,10 @@ const attackResult monster::attack(monster &target) {
   // Now to see how much damage we did...
   auto max = target.injury().max();
   int damage = target.wound(strength_.cur(), dt);
-  onHit(target, damage);
+  bool fatal = static_cast<unsigned char>(damage) == max;
+  if (!fatal) onHit(target, damage);
   if (damage == 0) return attackResult(injury(), L"ineffectual");
-  if (static_cast<unsigned char>(damage) == max) return attackResult(injury(), L"fatal");
+  if (fatal) return attackResult(injury(), L"fatal");
   if (static_cast<unsigned char>(damage) >= max/2) return attackResult(injury(), L"good hit");
   return attackResult(injury(), L"hit");
 }
